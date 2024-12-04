80571934642254d8c19d5f521d6c284dcb83333a
zt64 <31907977+zt64@users.noreply.github.com>
2024-12-03 23:45:23 -0500

    better main function

--- src/main/kotlin/Day.kt
+++ src/main/kotlin/Day.kt
@@ -1,4 +1,4 @@
-abstract class Day(val number: Int) {
+sealed class Day(val number: Int) {
     protected val input = javaClass.getResource(
         "day${number.toString().padStart(2, '0')}.txt"
     )!!.readText()
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,55 @@
 
 import kotlinx.coroutines.*
+import java.util.*
 import kotlin.time.measureTimedValue
 
-fun main(args: Array<String>) {
-    val day = args.firstOrNull()?.toIntOrNull()?.let { TODO() } ?: Day02
+val days = listOf<Day>(
+    Day01,
+    Day02,
+    Day03,
+    Day04,
+    // Day05,
+    // Day06,
+    // Day07,
+    // Day08,
+    // Day09,
+    // Day10,
+    // Day11,
+    // Day12,
+    // Day13,
+    // Day14,
+    // Day15,
+    // Day16,
+    // Day17,
+    // Day18,
+    // Day19,
+    // Day20,
+    // Day21,
+    // Day22,
+    // Day23,
+    // Day24,
+    // Day25
+)
 
-    printSolution(day)
+fun main(args: Array<String>) {
+    val calendar = Calendar.getInstance()
+
+    val day = if (calendar.get(Calendar.YEAR) == 2024) {
+        calendar.get(Calendar.DAY_OF_MONTH)
+    } else {
+        args.firstOrNull()?.toIntOrNull()
+    } ?: error("Day not found")
+
+    printSolution(days[day - 1])
 }
 
-fun printSolution(day: Day) {
+private fun printSolution(day: Day) {
     println("Day ${day.number}")
 
     runBlocking {
         val part1Job = launch {
             val progressJob = launchProgressIndicator("Processing Part 1")
-            val (part1, part1Duration) = measureTimedValue { day.part1() }
+            val (part1, part1Duration) = measureTimedValue(day::part1)
             progressJob.cancel()
             println("\rPart 1: (took $part1Duration) $part1")
         }
@@ -22,7 +57,7 @@ fun printSolution(day: Day) {
 
         val part2Job = launch {
             val progressJob = launchProgressIndicator("Processing Part 2")
-            val (part2, part2Duration) = measureTimedValue { day.part2() }
+            val (part2, part2Duration) = measureTimedValue(day::part2)
             progressJob.cancel()
             println("\rPart 2: (took $part2Duration) $part2")
         }
@@ -30,7 +65,7 @@ fun printSolution(day: Day) {
     }
 }
 
-fun CoroutineScope.launchProgressIndicator(message: String) = launch {
+private fun CoroutineScope.launchProgressIndicator(message: String) = launch {
     val progressChars = arrayOf("|", "/", "-", "\\")
     var index = 0
     while (isActive) {
