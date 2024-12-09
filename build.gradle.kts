5fb0adef2b0d875581882cccbe424f562566a79f
zt64 <31907977+zt64@users.noreply.github.com>
2024-12-08 23:53:23 -0500

    setup graalvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,34 @@
 plugins {
+    application
+
     alias(libs.plugins.kotlin)
     alias(libs.plugins.ktlint)
+    id("org.graalvm.buildtools.native") version "0.10.4"
 }
 
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass = "MainKt"
+}
+
+graalvmNative {
+    toolchainDetection = true
+
+    binaries {
+        named("main") {
+            javaLauncher.set(javaToolchains.launcherFor {
+                languageVersion.set(JavaLanguageVersion.of(21))
+                vendor.set(JvmVendorSpec.matching("Oracle"))
+            })
+
+            buildArgs("-Ob")
+
+            resources.autodetect()
+        }
+    }
 }
 
 dependencies {
