10daf67825f4faf53ad5455df74ef78e581ea154
zt64 <31907977+zt64@users.noreply.github.com>
2024-12-13 23:56:52 -0500

    clean up day 13

--- src/main/kotlin/day/Day13.kt
+++ src/main/kotlin/day/Day13.kt
@@ -1,5 +1,7 @@
 package day
 
+private typealias Game = Triple<Pair<Long, Long>, Pair<Long, Long>, Pair<Long, Long>>
+
 object Day13 : Day(13) {
     private val games = input.split("\n\n").map {
         val pairs = it.lines().map {
@@ -8,21 +10,15 @@ object Day13 : Day(13) {
                 .map { it.drop(2).toLong() }.let { it[0] to it[1] }
         }
 
-        Game(pairs[0], pairs[1], pairs[2])
+        Triple(pairs[0], pairs[1], pairs[2])
     }
-
-    private data class Game(
-        val buttonA: Pair<Long, Long>,
-        val buttonB: Pair<Long, Long>,
-        val prize: Pair<Long, Long>
-    )
 
     override fun part1(): Any = games.countTokens()
 
     override fun part2(): Any {
         return games.map { game ->
             game.copy(
-                prize = game.prize.let {
+                third = game.third.let {
                     it.first + 10000000000000 to it.second + 10000000000000
                 }
             )
@@ -31,13 +27,10 @@ object Day13 : Day(13) {
 
     // dont ask me how this works, cause i sure as hell dont know
     private fun List<Game>.countTokens(): Long {
-        return sumOf { game ->
-            val ax = game.buttonA.x
-            val ay = game.buttonA.y
-            val bx = game.buttonB.x
-            val by = game.buttonB.y
-            val px = game.prize.x
-            val py = game.prize.y
+        return sumOf { (buttonA, buttonB, prize) ->
+            val (ax, ay) = buttonA
+            val (bx, by) = buttonB
+            val (px, py) = prize
 
             val a = (by * px) + (-bx * py)
             // common denominator
@@ -55,7 +48,4 @@ object Day13 : Day(13) {
             buttonAPress * 3 + buttonBPress * 1
         }
     }
-
-    private val Pair<Long, Long>.x get() = first
-    private val Pair<Long, Long>.y get() = second
 }
