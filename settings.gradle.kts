31cb20918a5993a7fcb60c068498476ba2c1700c
zt64 <31907977+zt64@users.noreply.github.com>
2025-12-09 18:07:18 -0500

    setup foojay resolver plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,7 @@
 @file:Suppress("UnstableApiUsage")
 
+import org.gradle.toolchains.foojay.FoojayToolchainResolver
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
@@ -12,6 +14,20 @@ pluginManagement {
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver") version "1.0.0"
+}
+
+toolchainManagement {
+    jvm {
+        javaRepositories {
+            repository("foojay") {
+                resolverClass.set(FoojayToolchainResolver::class.java)
+            }
+        }
     }
 }
 
